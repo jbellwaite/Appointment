b8151f8fb872c587e38f53debd7d26cf9a91cb9b
Alan Falcon and Jessie Waite <alan@>
2017-09-13 16:39:23 -0700

    fix alert

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,17 @@
 
 $(document).ready(function() {
   $("#formOne").submit(function() {
-    event.preventDefault();
+
     var firstName1Input = $("input#firstName1").val();
     var lastName2Input = $("input#lastName2").val();
     var appointmentDescription1Input= $("input#appointmentDescription1").val();
     var appointmentDate1Input = $("input#appointmentDate1").val();
     var startTime1Input = $("input#startTime1").val();
     var endTime1Input = $("input#endTime1").val();
+    var message = ("Here is the information about your appointment: " + firstName1Input + " " + lastName2Input + "for "+ appointmentDescription1Input + " on " + appointmentDate1Input + " between " + startTime1Input + " and " + endTime1Input);
 
 //User interface (or front-end) logic
-
+    event.preventDefault();
     $(".firstName1").append(firstName1Input);
     $(".lastName2").append(lastName2Input);
     $(".appointmentDescription1").append(appointmentDescription1Input);
@@ -19,7 +20,7 @@ $(document).ready(function() {
     $(".startTime1").append(startTime1Input);
     $(".endTime1").append(endTime1Input);
 
-    $("#story").show();
-
+    alert(message);
+   //$("#story").show();
   });
 });
