2bc22abf7fdf9361e8919ff322c1e1a10afc3bb1
Jessie <jess.bell@me.com>
2017-09-13 21:39:16 -0700

    Change code to make survey functional

--- js/scripts.js
+++ js/scripts.js
@@ -5,20 +5,20 @@ $(document).ready(function() {
 
     var firstName1Input = $("input#firstName1").val();
     var lastName2Input = $("input#lastName2").val();
-    var appointmentDescription1Input= $("input#appointmentDescription1").val();
+    var favoritebevInput= $("#favoritebev").val();
     var appointmentDate1Input = $("input#appointmentDate1").val();
-    var startTime1Input = $("input#startTime1").val();
-    var endTime1Input = $("input#endTime1").val();
-    var message = ("Here is the information about your appointment: " + firstName1Input + " " + lastName2Input + "for "+ appointmentDescription1Input + " on " + appointmentDate1Input + " between " + startTime1Input + " and " + endTime1Input);
+    var flavor = $("input:radio[name=flavor]:checked").val();
+
+    var message = ("Thank you for taking our survey. Here are your results: Your name is " + firstName1Input + " " + lastName2Input + ", and you love "+ favoritebevInput + ". You also prefer " + appointmentDate1Input + " and " + flavor);
 
 //User interface (or front-end) logic
     event.preventDefault();
-    $(".firstName1").append(firstName1Input);
-    $(".lastName2").append(lastName2Input);
-    $(".appointmentDescription1").append(appointmentDescription1Input);
-    $(".appointmentDate1").append(appointmentDate1Input);
-    $(".startTime1").append(startTime1Input);
-    $(".endTime1").append(endTime1Input);
+    $(".firstName1").add(firstName1Input);
+    $(".lastName2").add(lastName2Input);
+    $(".favoritebev").add(favoritebevInput);
+    $(".appointmentDate1").add(appointmentDate1Input);
+    $(".radio").add(flavor);
+
 
     alert(message);
    //$("#story").show();
